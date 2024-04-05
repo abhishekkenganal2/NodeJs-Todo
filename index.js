8e1e93a7612e42e50c3f674eefbd60e76bbd4d85
Abhishekb <abhishekkenganal6789@gmail.com>
2024-04-05 07:39:12 +0530

    dncad

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express")
-
+const mongoose = require("mongoose")
 const PORT = 8000
 
 //init app
 const app = express();
+
+const connectURL =  "mongodb://localost:27017/todoDb"
 
 //view engine
 app.set("view engine","ejs");
