103efc8f258e9b5f0ff8c57b863f6f42753ff9e7
Abhishekb <abhishekkenganal6789@gmail.com>
2024-04-05 09:29:34 +0530

    dsdmq

--- index.js
+++ index.js
@@ -1,17 +1,56 @@
-const express = require("express")
-const mongoose = require("mongoose")
-const PORT = 8000
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path")
+
+const PORT = 8000;
 
 //init app
 const app = express();
 
-const connectURL =  "mongodb://localost:27017/todoDb"
+const connectURL = "mongodb://localost:27017/todoDb";
+
+mongoose
+  .connect(connectURL)
+  .then(() => console.log("database connected"))
+  .catch((error) => console.log(error.message));
 
 //view engine
-app.set("view engine","ejs");
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname,"public"))   )
 
-app.listen(PORT,()=>{
-    console.log(`server run at ${PORT}`)
+//get route
+app.get("/",(req,res,next)=> {
+    try{
+        res.render("index",{title: "List todo"})
+    } catch(error){
+        res.status(500).json({message:error.message})
+    }
 })
 
+app.get("/add-todo",(req,res,next)=> {
+    try{
+        res.render("newTodo")
+    }catch(error){
+        res.status(500).json({message:error.message})
+    }
+})
 
+app.get("/update-todo",(req,res,next)=> {
+    try{
+        res.render("updateTodo")
+    }catch(error){
+        res.status(500).json({message:error.message})
+    }
+})
+
+app.get("/delete-todo",(req,res,next)=> {
+    try{
+        res.render("delete")
+    }catch(error){
+        res.status(500).json({message:error.message})
+    }
+})
+
+app.listen(PORT, () => {
+  console.log(`server run at ${PORT}`);
+});
